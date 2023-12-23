e69809c2f87377ff64d76e6d3d92a7c33a1bf851
Hamza Juwaihan <hamza.zaher12@gmail.com>
2023-12-23 15:44:27 +0300

    finish bonus tasks

--- routers/minions/minionsRouter.js
+++ routers/minions/minionsRouter.js
@@ -24,9 +24,8 @@ minionsRouter.post("/", (req, res, next) => {
 
 minionsRouter.param("minionId", (req, res, next, id) => {
   const minionId = id;
-  console.log(minionId);
   const minion = getFromDatabaseById("minions", minionId);
-  console.log(req.minion);
+
   if (minion) {
     req.minion = minion;
     next();
@@ -36,7 +35,6 @@ minionsRouter.param("minionId", (req, res, next, id) => {
 });
 
 minionsRouter.get("/:minionId", (req, res, next) => {
-
   if (req.minion) {
     res.send(req.minion);
   } else {
@@ -61,5 +59,55 @@ minionsRouter.delete("/:minionId", (req, res, next) => {
     res.status(404).send();
   }
 });
+
+minionsRouter.get("/:minionId/work", (req, res, next) => {
+  const work = getAllFromDatabase("work").filter(
+    (singleWork) => singleWork.minionId === req.minion.id
+  );
+  if (work) {
+    res.send(work);
+  } else {
+    res.status(404).send();
+  }
+});
+
+minionsRouter.post("/:minionId/work", (req, res, next) => {
+  const newWork = req.body;
+  newWork.minionId = req.minion.id;
+  const createdWork = addToDatabase("work", newWork);
+  res.status(201).send(createdWork);
+});
+
+minionsRouter.param("workId", (req, res, next, id) => {
+  const workId = id;
+  const work = getFromDatabaseById("work", workId);
+
+  if (work) {
+    req.work = work;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+minionsRouter.put("/:minionId/work/:workId", (req, res, next) => {
+  if (req.minion && req.work && req.minion.id === req.work.minionId) {
+    
+    const updatedWork = updateInstanceInDatabase("work", req.body);
+    res.send(updatedWork);
+  } else {
+    res.status(400).send();
+  }
+});
+
+minionsRouter.delete("/:minionId/work/:workId", (req, res, next) => {
+  if (req.minion && req.work) {
+    deleteFromDatabasebyId("work", req.work.id);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
+
 
 module.exports = minionsRouter;
